5cdeecb06707fc2544cc9e909a4d18963e327fda
Rasba Hozan <rabsahozan426@gmail.com>
2024-08-27 01:40:42 -0700

    code for swapping elements of array

---
+++ array problems/training/java/emumba/SwapElement.java
@@ -0,0 +1,16 @@
+package training.java.emumba;
+
+public class SwapElement {
+    public static void main(String[] args) {
+        int[] array = {23, 34, 12};
+int a = 0;
+int b = 2;
+        // Swapping elements at position a and b
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+ " ");
+        }
+    }
+}
