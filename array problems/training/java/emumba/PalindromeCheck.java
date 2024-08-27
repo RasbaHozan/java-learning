0ad17f24277110deca644f6ec2b44cbd856c6f79
Rasba Hozan <rabsahozan426@gmail.com>
2024-08-27 00:44:20 -0700

    code for palindrome check.java

---
+++ array problems/training/java/emumba/PalindromeCheck.java
@@ -0,0 +1,21 @@
+package training.java.emumba;
+
+public class PalindromeCheck {
+public static boolean isPalindrome(int[] array) {
+	for ( int i=0; i < (array.length/2) ; i++) {
+		if (array[i] != array[array.length-1-i]) {
+			return false;
+		}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int[] array= {2,5,3,2};
+		if(isPalindrome(array)) {
+			System.out.println("array is palindrome");
+		}else {
+			System.out.println("array is not a palindrome");
+		}
+	}
+}
