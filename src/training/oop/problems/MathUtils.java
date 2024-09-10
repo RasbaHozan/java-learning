1525fd4b638a37e96c5f67312bdf5a0eaf5122fa
Rasba Hozan <rabsahozan426@gmail.com>
2024-09-10 08:36:13 -0700

    Implement basic bankAccount functionality with deposit ,withdraw and checkBalance methods

--- src/training/oop/problems/MathUtils.java
+++ src/training/oop/problems/MathUtils.java
@@ -3,13 +3,16 @@ package training.oop.problems;
 public class MathUtils {
 int x;
 int y;
+
 MathUtils(int x ,int y ){
 	this.x=x;
 	this.y=y;
 }
+
  public int add() {
 	return this.x+this.y;
 }
+ 
  public int multiply() {
 	 return this.x* this.y;
  }
